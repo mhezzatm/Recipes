ed26281d3bb000db020bcabe83d2bc9e623bd0f4
Mohamed <102462883+mhezzatm@users.noreply.github.com>
2026-08-28 12:31:37 +0300

    i think its final

---
+++ src/assets/lists/tags/breakfast/code.js
@@ -0,0 +1,17 @@
+const url = "https://tasty.p.rapidapi.com/recipes/list?from=0&size=900&tags=breakfast";
+const options = {
+  method: "GET",
+  headers: {
+    "content-type": "application/octet-stream",
+    "X-RapidAPI-Key": "6fe2cef339msh30d7a18b8ca5bdcp1f6234jsn3a1022079a5c",
+    "X-RapidAPI-Host": "tasty.p.rapidapi.com",
+  },
+};
+
+try {
+  const response = await fetch(url, options);
+  const result = await response.text();
+  console.log(result);
+} catch (error) {
+  console.error(error);
+}
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="relative">
+    <SlideShow />
+    <HomeBody />
+  </div>
+</template>
+
+<script>
+import HomeBody from "@/components/home/HomeBody.vue";
+import SlideShow from "@/components/home/SlideShow.vue";
+
+export default {
+  components: { SlideShow, HomeBody },
+};
+</script>
+
+<style scoped></style>
